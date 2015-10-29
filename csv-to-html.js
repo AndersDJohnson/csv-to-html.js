546c622b2bcc9bd408996b83b405e111a038fc1a
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-10-28 23:54:24 -0500

    refactor

--- csv-to-html.js
+++ csv-to-html.js
@@ -1,25 +1,25 @@
 $(function() {
-	var $templateData = $("#templateData");
-	var h = $templateData.html();
-	
-	// dupe commas
-	//while (h.indexOf(",,") > -1) {
-	//	h = h.replace(/\,\,/g,',');
-	//}
-	
-	// last comma
-	h = h.replace(/\,\n/g,'\n');
-	
-	// 
-	h = h.replace(/\n/g,'</td></tr>\n<tr><td>');
-	
-	// table elements
-	h = h.replace(/\,/g,'</td><td>');
-	
-	h = "<table><tr><td>" + h + "</td></tr></table>";
-	
-	// insert guid links
-	h = h.replace(/([a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+)/g, "<a href='full_demo.jsp?templateId=$1'>$1</a>");
-	$templateData.parent().append(h);
-	$templateData.remove();
+	var $els = $(".csv-to-html");
+	$els.each(function () {
+		var $el = $(this);
+		var h = $el.html();
+		// dupe commas
+		//while (h.indexOf(",,") > -1) {
+		//	h = h.replace(/\,\,/g,',');
+		//}
+		
+		// last comma
+		h = h.replace(/\,\n/g,'\n');
+		
+		// 
+		h = h.replace(/\n/g,'</td></tr>\n<tr><td>');
+		
+		// table elements
+		h = h.replace(/\,/g,'</td><td>');
+		
+		h = "<table><tr><td>" + h + "</td></tr></table>";
+		
+		$el.parent().append(h);
+		$el.remove();
+	});
 });
